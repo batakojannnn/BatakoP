1a5e99ded3428117c60e6cf77321ffacb851d1c9
EC2 Default User <ec2-user@ip-172-31-12-119.ec2.internal>
2022-10-13 06:32:06 +0000

    for Heroku

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '26a07816cfaa0d7e028aced0d99f2c26420590e6922d68dbec33cb8a67b05edb610b3facf4c30e0bb6b52249b1b4abd71fa777bc345327cbd2ce5ad0b2ebbb64'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root to: 'home#top'
+
+  devise_for :users
+
+  get 'home/about' => 'home#about', as:'about'
+
+  resources :books, only: [:new, :create, :edit, :update, :index, :show, :destroy]
+  resources :users, only: [:show, :index, :edit, :update]
+end
